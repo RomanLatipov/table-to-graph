fe387afa8db93c27113e5ecdd773664508431c47
RomanLatipov <romanlatipov@gmail.com>
2024-07-23 01:25:05 -0400

    complete table-to-graph lab

--- javascript/table_to_graph.js
+++ javascript/table_to_graph.js
@@ -1,5 +1,32 @@
 function tableToGraph(friends) {
-  // type your code here
+  friends = friends.replaceAll("<tr>", "")
+  friends = friends.replaceAll("</tr>", "")
+  const friendsCp = friends.substring(friends.indexOf("<td>"), friends.lastIndexOf("</td>")+5);
+  const temp = friendsCp.split('</td>');
+  temp.pop();
+  for (let i=0; i<temp.length; i++) {
+    temp[i] = temp[i].replace("<td>", "")
+  }
+  const obj = {};
+  for (let i=0; i<temp.length-1; i=i+2) {
+    if (!obj[temp[i]]) {
+      obj[temp[i]] = [];
+    }
+    let tempArr = temp[i+1].split(", ");
+    for (let j=0; j<tempArr.length; j++) {
+      if (!obj[temp[i]].includes((tempArr[j])))
+        obj[temp[i]].push(tempArr[j])
+      if (!obj[tempArr[j]])
+        obj[tempArr[j]] = [temp[i]];
+      else {
+        if (!obj[tempArr[j]].includes((temp[i])))
+          obj[tempArr[j]].push(temp[i]);
+      }
+        
+    }
+  }
+  console.log(obj)
+  // return obj;
 }
 
 if (require.main === module) {
@@ -19,12 +46,13 @@ if (require.main === module) {
     Xi: ["Fred"],
     Janelle: ["Carol"]
   };
-
-  console.log("Expecting: ");
-  console.log(printResults(result));
-  console.log("");
-  console.log("Got: ");
-  console.log(printResults(tableToGraph(friends)));
+  let friends1 = "<table><tr><th>Person</th><th>Friends</th></tr><tr><td>Gremlin</td><td></td></tr></table>";
+  // console.log("Expecting: ");
+  // console.log(printResults(result));
+  // console.log("");
+  // console.log("Got: ");
+  // console.log(printResults(tableToGraph(friends)));
+  tableToGraph(friends1)
 
   console.log("");
 }
